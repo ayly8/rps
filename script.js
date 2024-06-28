ce7994ddfbbec4276d2413357429d0105fffd619
ayly8 <allison.y.ly@gmail.com>
2024-06-27 21:19:41 -0700

    updated logic and fixed score error

--- script.js
+++ script.js
@@ -1,76 +1,55 @@
+let humanScore = 0;
+let computerScore = 0;
+let numRounds = 0;
+
 function getComputerChoice() {
-   return Math.floor(Math.random() * 3) + 1;
+   const choices = ['rock', 'paper', 'scissors'];
+   const randomChoice = Math.floor(Math.random() * 3);
+   return choices[randomChoice];
 }
 
-function getHumanChoice() {
-   let user_choice = 0;
-   if (document.getElementById("rock_btn")) {
-      user_choice = 1;
-   } else if (document.getElementById("paper_btn")) {
-      user_choice = 2;
-   } else if (document.getElementById("scissor_btn")) {
-      user_choice = 3;
+function playGame(humanChoice) {
+   //update corresponding computer choice image
+   const computerChoice = getComputerChoice();
+   if (computerChoice == 'rock') {
+      document.getElementById("cpu_choice").src = "stone.png";
+   } else if (computerChoice == 'paper') {
+      document.getElementById("cpu_choice").src = "paper.png";
+   } else if (computerChoice == 'scissors') {
+      document.getElementById("cpu_choice").src = "scissor.png";
    }
-   return user_choice;
-}
 
-function playGame() {
-   let humanScore = 0;
-   let computerScore = 0;
-   let numRounds = 0;
-   const humanChoice = getHumanChoice()
-   const computerChoice = getComputerChoice()
+   //perform game logic and update outcome message accordingly
+   let outcome = '';
+   if (humanChoice == computerChoice) {
+      outcome = "It's a tie!";
+   } else if (
+      (humanChoice == 'rock' && computerChoice == 'scissors') ||
+      (humanChoice == 'paper' && computerChoice == 'rock') ||
+      (humanChoice == 'scissors' && computerChoice == 'paper')
+   ) {
+      humanScore++;
+      outcome = `You win, ${humanChoice} beats ${computerChoice}`;
+   } else {
+      computerScore++;
+      outcome = `You lose, ${computerChoice} beats ${humanChoice}`;
+   }
 
-   while (numRounds < 5) {
-      if (humanChoice > computerChoice) {
-         if (humanChoice == 2 && computerChoice == 1) {
-            //if humanChoice = paper and computerChoice = rock
-            document.getElementById("cpu_choice").src="stone.png";
-            humanScore++;
-            document.getElementById("outcome_msg").innerHTML = "You win, paper beats rock!"
-            document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
-         } else if (humanChoice == 3 && computerChoice == 2) {
-            //if humanChoice = scissor and computerChoice = paper
-            document.getElementById("cpu_choice").src="paper.png";
-            humanScore++;
-            document.getElementById("outcome_msg").innerHTML = "You win, scissor beats paper!"
-            document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
-         } else if (humanChoice == 3 && computerChoice == 1) {
-            //if humanChoice = scissor and computerChoice = rock
-            document.getElementById("cpu_choice").src="stone.png";
-            computerScore++
-            document.getElementById("outcome_msg").innerHTML = "You lose, rock beats scissor!"
-            document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
-         }
-      } else if (humanChoice < computerChoice) {
-         if (humanChoice == 1 && computerChoice == 2) {
-            //if humanChoice = rock and computerChoice = paper
-            document.getElementById("cpu_choice").src="paper.png";
-            computerScore++;
-            document.getElementById("outcome_msg").innerHTML = "You lose, paper beats rock!"
-            document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
-         } else if (humanChoice == 1 && computerChoice == 3) {
-            //if humanChoice = rock and computerChoice = scissor
-            document.getElementById("cpu_choice").src="scissor.png";
-            humanScore++;
-            document.getElementById("outcome_msg").innerHTML = "You win, rock beats scissor!"
-            document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
-         } else if (humanChoice == 2 && computerChoice == 3) {
-            //if humanChoice = paper and computerChoice = scissor
-            document.getElementById("cpu_choice").src="scissor.png";
-            computerScore++
-            document.getElementById("outcome_msg").innerHTML = "You lose, scissor beats paper!"
-            document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
-         }
-      } else if (humanChoice == computerChoice) {
-         document.getElementById("outcome_msg").innerHTML = "Tie!"
-         document.getElementById("score_msg").innerHTML = "Human: ${humanScore} | Computer: ${computerScore}";
+   //update HTML with correct messages
+   document.getElementById("outcome_msg").innerHTML = outcome;
+   document.getElementById("score_msg").innerHTML = `Human: ${humanScore} | Computer: ${computerScore}`;
+
+   //increase number of rounds
+   numRounds++;
+
+   //check number of rounds and update end message correctly
+   if (numRounds >= 5) {
+      if (humanScore > computerScore) {
+          document.getElementById("end_msg").innerHTML = "You won best out of 5 rounds!";
+      } else if (humanScore < computerScore) {
+          document.getElementById("end_msg").innerHTML = "You lost best out of 5 rounds!";
+      } else {
+          document.getElementById("end_msg").innerHTML = "You tied best of 5!";
       }
-      numRounds++;
-   }
-   if (humanScore > computerScore) {
-      document.getElementById("end_msg").innerHTML = "End of game, you won best of 5 rounds!"
-   } else if (humanScore < computerScore) {
-      document.getElementById("end_msg").innerHTML = "End of game, you lost best of 5 rounds"
-   }
+  }
 }
